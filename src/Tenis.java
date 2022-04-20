1a0c953ca51344542038ccf38b0b3537236567ae
Lucas Stecher <lucas.oliveira@coopersystem.com.br>
2022-04-20 20:40:13 -0300

    :tada: initial commit, added Tenis class

---
+++ src/Tenis.java
@@ -0,0 +1,35 @@
+public class Tenis {
+    private String nome;
+    private double tamanho;
+    private String cor;
+
+    public Tenis(String nome, double tamanho, String cor) {
+        this.nome = nome;
+        this.tamanho = tamanho;
+        this.cor = cor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(double tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+}
---
+++ src/TesteTenis.java
@@ -0,0 +1,6 @@
+public class TesteTenis {
+    public static void main(String[] args) {
+        Tenis adidas = new Tenis("Vulc", 40, "Branco");
+        System.out.println(adidas.getCor());
+    }
+}
