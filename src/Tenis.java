dd04c875c50134b3c7b42b770c0c7cc1de3afef0
Lucas Stecher <lucas.oliveira@coopersystem.com.br>
2022-04-21 15:16:02 -0300

    :sparkles: added inheritance

--- src/Tenis.java
+++ src/Tenis.java
@@ -1,35 +1,6 @@
-public class Tenis {
-    private String nome;
-    private double tamanho;
-    private String cor;
+public class Tenis extends Calcado{
 
     public Tenis(String nome, double tamanho, String cor) {
-        this.nome = nome;
-        this.tamanho = tamanho;
-        this.cor = cor;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public double getTamanho() {
-        return tamanho;
-    }
-
-    public void setTamanho(double tamanho) {
-        this.tamanho = tamanho;
-    }
-
-    public String getCor() {
-        return cor;
-    }
-
-    public void setCor(String cor) {
-        this.cor = cor;
+        super(nome, tamanho, cor);
     }
 }
--- src/TesteTenis.java
+++ src/TesteTenis.java
@@ -1,6 +1,6 @@
 public class TesteTenis {
     public static void main(String[] args) {
-        Tenis adidas = new Tenis("Vulc", 40, "Branco");
+        Calcado adidas = new Calcado("Vulc", 40, "Branco");
         System.out.println(adidas.getCor());
     }
 }
