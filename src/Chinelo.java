1e175f4e5d40f05799c3d3631f3aec34bb8a730e
Lucas Stecher <lucas.oliveira@coopersystem.com.br>
2022-04-22 21:55:28 -0300

    :sparkles: added abstract class, along with some comments

--- src/Chinelo.java
+++ src/Chinelo.java
@@ -1,5 +1,7 @@
-public class Chinelo extends Calcado{
-    public Chinelo(String nome, double tamanho, String cor, String material) {
-        super(nome, tamanho, cor, material);
-    }
+public abstract class Chinelo {
+    // Classe chinelo é abstrata, um 'conceito'.
+    // Classes abstratas podem ter métodos abstratos.
+    // Classes abstratas podem ter classes filhas.
+    // Classes e métodos abstratos consomem menos memória, logo, melhoram o desempenho do nosso programa.
+    public abstract void  isRemendado();
 }
---
+++ src/Havaianas.java
@@ -0,0 +1,7 @@
+public class Havaianas extends  Chinelo {
+    // Extende a classe abstrata Chinelo, junto de seu método
+    // Método precisa ser declarado
+    public void isRemendado(){
+        System.out.println("Sempre");
+    }
+}
